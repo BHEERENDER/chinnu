e149c014156d60d67fdde311889f3dd26d8e4f79
bheerender <bheerender123@gmail.com>
2020-01-02 17:23:01 +0530

    angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public x="Friends";
   constructor() { }
 
   ngOnInit() {
+   
   }
+onclick()
+{
+  this.x = this.x+"...Bheerender";
+  this.x = this.x+"...Bhanu";
+  this.x = this.x+"...Sha";
+  this.x = this.x+"...Santhu";
+  this.x = this.x+"...harshu";
+
+}
 
 }
